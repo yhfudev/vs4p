60632f69eb1a6b8b1ff5fcd07ff0873a4f821d17
woodsdog <woodsdog@1e377bde-c63b-0410-a541-41a2a99b7c37>
2007-10-26 00:48:13 +0000

    Added help to all of the scripts

--- iphone/video-to-iphone.sh
+++ iphone/video-to-iphone.sh
@@ -1,7 +1,52 @@
 #!/bin/sh
 
-QUALITY=400k
-THREADS=1
+if [ $# -ne 1 ]; then
+   echo "usage: $(basename $0) <videofile>"
+   exit 1
+fi
+
+while getopts "b:t:h" flag
+do
+   case $flag in
+      b )
+         BITRATE="$OPTARG"
+      ;;
+      t )
+         THREADS="$OPTARG"
+      ;;
+      h )
+         echo "Usage: `basename $0` [-b bitrate] [-t numOfThreads] [-h]"
+         echo "    -b   Specify the Bitrate.  Should be between 200k and 1500k."
+         echo "         Note: The \"k\" is needed.  e.g. \"512k\""
+         echo "    -h   help (this help message)"
+         echo "    -t   Specify the number of threads. This will default to the number of CPUS found."
+         echo ""
+         echo "example:  `basename $0` -b 512k -t 2 some_video_file.avi"
+         exit 1
+      ;;
+      \? )
+         echo "Usage: `basename $0` [-b bitrate] [-t numOfThreads] [-h]"
+         echo "Use \"`basename $0` -h\" for more information"
+         exit 1
+      ;;
+      * )
+         echo "Usage: `basename $0` [-b bitrate] [-t numOfThreads] [-h]"
+         echo "Use \"`basename $0` -h\" for more information"
+         exit 1
+      ;;
+   esac
+done
+
+if [ -z "$BITRATE" ] 
+then
+   BITRATE=300k
+fi
+
+if [ -z "$THREADS" ] 
+then
+   THREADS=$(grep -c "^processor" /proc/cpuinfo)
+fi
+
 TITLE=$(echo $PWD |awk -F/ '{print $(NF)}')
 
 if [ -e ./.CONVERTED-TO-IPHONE ]
@@ -12,9 +57,9 @@ then
 fi
 
 #Pass 1
-ffmpeg -threads $THREADS -y -i $1 -s 480x272 -vcodec libx264 -b $QUALITY -flags +loop -cmp +chroma -me_range 16 -g 300 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -rc_eq "blurCplx^(1-qComp)" -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -coder 0 -refs 1 -bt $QUALITY -maxrate 4M -bufsize 4M -level 21  -r 30000/1001 -partitions +parti4x4+partp8x8+partb8x8 -me hex -subq 5 -f mp4 -aspect 480:272 -title "$TITLE" -acodec libfaac -ac 2 -ar 48000 -ab 128 -pass 1 "$TITLE.mp4"
+ffmpeg -threads $THREADS -y -i $1 -s 480x272 -vcodec libx264 -b $BITRATE -flags +loop -cmp +chroma -me_range 16 -g 300 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -rc_eq "blurCplx^(1-qComp)" -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -coder 0 -refs 1 -bt $BITRATE -maxrate 4M -bufsize 4M -level 21  -r 30000/1001 -partitions +parti4x4+partp8x8+partb8x8 -me hex -subq 5 -f mp4 -aspect 480:272 -title "$TITLE" -acodec libfaac -ac 2 -ar 48000 -ab 128 -pass 1 "$TITLE.mp4"
 #Pass 2
-ffmpeg -threads $THREADS -y -i $1 -s 480x272 -vcodec libx264 -b $QUALITY -flags +loop -cmp +chroma -me_range 16 -g 300 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -rc_eq "blurCplx^(1-qComp)" -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -coder 0 -refs 1 -bt $QUALITY -maxrate 4M -bufsize 4M -level 21  -r 30000/1001 -partitions +parti4x4+partp8x8+partb8x8 -me hex -subq 5 -f mp4 -aspect 480:272 -title "$TITLE" -pass 2 -acodec libfaac -ac 2 -ar 48000 -ab 128 -vol 320 "$TITLE.mp4"
+ffmpeg -threads $THREADS -y -i $1 -s 480x272 -vcodec libx264 -b $BITRATE -flags +loop -cmp +chroma -me_range 16 -g 300 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -rc_eq "blurCplx^(1-qComp)" -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -coder 0 -refs 1 -bt $BITRATE -maxrate 4M -bufsize 4M -level 21  -r 30000/1001 -partitions +parti4x4+partp8x8+partb8x8 -me hex -subq 5 -f mp4 -aspect 480:272 -title "$TITLE" -pass 2 -acodec libfaac -ac 2 -ar 48000 -ab 128 -vol 320 "$TITLE.mp4"
 if [ $? -eq 0 ]
 then
    touch .CONVERTED-TO-IPHONE
